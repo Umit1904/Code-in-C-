f38ae71f5a802843f1a1259d46caabc3f2123a21
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-19 13:39:25 +0600

    Create main7.cpp

---
+++ main7.cpp
@@ -0,0 +1,15 @@
+
+//7
+#include <iostream>
+using namespace std;
+double sum(double firstNumber, double secondNumber){
+    return firstNumber+secondNumber;
+}
+int main(){
+    double firstNumber=0.5;
+    double secondNumber=0.25;
+    cout<<sum(firstNumber, secondNumber);
+return 0;
+    
+}
+
