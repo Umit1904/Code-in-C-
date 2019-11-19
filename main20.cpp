7cfcf007729901de5cac74aa9002f4710a2b2969
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-19 13:49:44 +0600

    Create main20.cpp

---
+++ main20.cpp
@@ -0,0 +1,11 @@
+//20
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    double minutes, distance;
+    cin>>minutes>>distance;
+    double speed = (distance*1000)/(minutes*60);
+    cout<<speed<<"m/sec"<<endl;
+    return 0;
+}
+    
