54d0c25e5b781eaf18698a46f2f7fce5e1722c19
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-19 13:35:24 +0600

    Create main50.cpp

---
+++ main50.cpp
@@ -0,0 +1,11 @@
+//50
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    int number1, number2, number3;
+    cin>>number1>>number2>>number3;
+    if(number1==number2 ||  number1==number3 || number2==number3){
+        cout<<"YES"<<endl;
+    }
+    return 0;
+}
