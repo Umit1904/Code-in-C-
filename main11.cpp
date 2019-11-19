1a9950f7924d6ed85000743af2aef6d9901566b0
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-19 13:41:40 +0600

    Create main11.cpp

---
+++ main11.cpp
@@ -0,0 +1,12 @@
+
+//11
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    double x=-2.34;
+    double result;
+    result=((abs(x-5)-sin(x))/3)+sqrt(x*x+2014)*cos(2*x)-3;
+    cout<<result<<endl;
+    return 0;
+}
+
