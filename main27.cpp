c62ae1e2e97d15c90fd1e8801e0045ac08a1ab94
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-19 13:53:28 +0600

    Create main27.cpp

---
+++ main27.cpp
@@ -0,0 +1,13 @@
+//27
+#include <iostream>
+using namespace std;
+int main(){
+    int a1, b1, c1;
+    cin>>a1>>b1>>c1;
+    int a=a1+b1;
+    int b=c1-a1;
+    int c=a1+b1+c1;
+    cout<<a<<" "<<b<<" "<<c;
+    return 0;
+    
+}
