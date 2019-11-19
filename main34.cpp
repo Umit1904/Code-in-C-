1356b807cf1a7a81451fde2e885a8e3e6e006707
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-19 13:25:14 +0600

    Create main34.cpp

---
+++ main34.cpp
@@ -0,0 +1,16 @@
+//34
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    int a;
+    cin>>a;
+    if(a>3){
+        a+=10;
+    }
+    else{
+        a=a-10;
+    }
+    cout<<a<<endl;
+    return 0;
+}
+
