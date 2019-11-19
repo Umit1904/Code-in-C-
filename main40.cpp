1a5bd07fdae2537b1bbf8779fb84fc411f188586
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-19 13:29:05 +0600

    Create main40.cpp

---
+++ main40.cpp
@@ -0,0 +1,15 @@
+
+//40
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    int number;
+    cin>>number;
+    if(number>-10 && number<10){
+        cout<<number+5<<endl;
+    }
+    else{
+        cout<<number-10<<endl;
+    }
+    return 0;
+}
