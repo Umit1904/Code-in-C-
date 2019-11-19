740d5c1f530e58a33259aff38fc139123a439e7d
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-19 13:50:53 +0600

    Create main22.cpp

---
+++ main22.cpp
@@ -0,0 +1,11 @@
+//22
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int tempC;
+    cin>>tempC;
+    double tempF=tempC*(1.8)+32;
+    cout<<"In Fahrenheit= "<<tempF<<"F";
+    return 0;
+}
+
