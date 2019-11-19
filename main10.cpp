af1e3bec3bd38bb790d14078f6026075af629af9
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-19 13:41:03 +0600

    Create main10.cpp

---
+++ main10.cpp
@@ -0,0 +1,14 @@
+
+//10
+#include <iostream>
+using namespace std;
+void result(double x){
+    cout<<(x+1)*(x+1) +3*(x+1)<<endl; 
+}
+int main(){
+    double x1= 1.7;
+    double x2= 3;
+    result(x1);
+    result(x2);
+    return 0;
+}
