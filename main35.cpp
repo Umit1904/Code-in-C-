1f4fa1f7a2aebf2448fb6a13693ba63b2846ef6e
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-19 13:25:46 +0600

    Create main35.cpp

---
+++ main35.cpp
@@ -0,0 +1,17 @@
+//35
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    int a;
+    cin>>a;
+    if(a<7){
+        cout<<"YES"<<endl;
+    }
+    else if(a>10){
+        cout<<"NO"<<endl;
+    }
+    else if(a==9){
+        cout<<"ERROR"<<endl;
+    }
+    return 0;
+}
