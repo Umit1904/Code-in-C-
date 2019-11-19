2a14cde8b9a0469d6a1a433764e34e251305a9a4
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-19 13:44:12 +0600

    Create main15.cpp

---
+++ main15.cpp
@@ -0,0 +1,11 @@
+//15
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int somenumber;
+    cin>>somenumber;
+    cout<< pow(somenumber, 2)<<endl;
+    cout<< pow(somenumber, 3);
+    return 0;
+}
