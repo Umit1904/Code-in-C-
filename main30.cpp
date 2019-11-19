8a18c89aedc5e4dfb1cbf533575dcefe7c4ebc5c
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-19 13:23:19 +0600

    Create main30.cpp

---
+++ main30.cpp
@@ -0,0 +1,13 @@
+//30
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    int a;
+    cin>>a;
+    cout<<pow(a,2)*pow(a,2)<<endl;
+    cout<<pow(a,2)*pow(a,2)*pow(a,2)<<endl;
+    cout<<pow(a,3)*pow(a,3)*pow(a,3)*pow(a,3)*pow(a,3);
+    return 0;
+}
+
+
