1279823f7279c9ce958d0f86067a3ccca46ac858
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-19 12:35:51 +0600

    Create main2.cpp

---
+++ main2.cpp
@@ -0,0 +1,12 @@
+//2 
+#include <iostream>
+using namespace std;
+int main (){
+    string day="Monday";
+    string month="November";
+    string name="Umit";
+    cout<<day<<endl;
+    cout<<month<<endl;
+    cout<<name<<endl;
+    return 0;
+}
