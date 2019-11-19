6616584133d5dad1f97635402606b613481b6e75
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-19 13:50:17 +0600

    Create main21.cpp

---
+++ main21.cpp
@@ -0,0 +1,14 @@
+//21
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    double a, b;
+    cin>>a>>b;
+    double c = sqrt(a*a + b*b);
+    cout<<"Area= "<<(a*b)/2<<endl;
+    cout<<"Perimeter= "<<a + b + c<< endl;
+    cout<<"Hypotenuse= "<<c;
+    return 0;
+    
+}
+    
