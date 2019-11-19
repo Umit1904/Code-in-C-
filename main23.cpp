0fdb8d6e42d3cfabbcd35d374060241525ee6172
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-19 13:51:24 +0600

    Create main23.cpp

---
+++ main23.cpp
@@ -0,0 +1,13 @@
+//23
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    double perKgCand, x, k;
+    cin>>perKgCand>>x>>k;
+    double a=x*perKgCand;
+    double kg=k/perKgCand;
+    cout<<a<<" dollars"<<endl;
+    cout<<kg<<" kg";
+    return 0;
+}
+
