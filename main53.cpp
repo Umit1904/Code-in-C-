bb6edc69cf6c6c8733dbbf6fc2a4648b94a5c09d
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-21 09:10:03 +0600

    Create main53.cpp

---
+++ main53.cpp
@@ -0,0 +1,14 @@
+//53
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n1, n2;
+    cin>>n1>>n2;
+    if(n1>30 || n2>30){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+    return 0;
+}
