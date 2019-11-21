3e73abe601a2ed7aaa2cd9e2df57089d55fde831
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-21 09:34:11 +0600

    Create main91.cpp

---
+++ main91.cpp
@@ -0,0 +1,12 @@
+//91
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    double number=0.0;
+    for(int i=2; i<=9; i++){
+        number+=((double )i/double(i+1));
+    }
+    cout<<number;
+    system("pause");
+    return 0;
+}
