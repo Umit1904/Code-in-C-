4922af64f6c509b00e7fc5983cd740e74163cd80
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-22 23:41:27 +0600

    Create main146.cpp

---
+++ main146.cpp
@@ -0,0 +1,43 @@
+//146
+#include <iostream>
+using namespace std;
+int main (){
+    int number=97; 
+    char letter;
+ 
+    for (int i=0; i<5; i++) {
+        letter=(char)number;
+        cout << letter <<" "; 
+        number++;
+    }cout<<endl;
+    for (int i=5; i<10; i++) {
+        letter=(char)number;
+        cout << letter <<" "; 
+        number++;
+    }cout<<endl;
+    for (int i=10; i<15; i++) {
+        letter=(char)number;
+        cout << letter <<" "; 
+        number++;
+    }cout<<endl;
+    for (int i=15; i<20; i++) {
+        letter=(char)number;
+        cout << letter <<" "; 
+        number++;
+    }cout<<endl;
+    for (int i=20; i<25; i++) {
+        letter=(char)number;
+        cout << letter <<" "; 
+        number++;
+    }cout<<endl;
+    for (int i=25; i<26; i++) {
+        letter=(char)number;
+        cout << letter <<" "; 
+        number++;
+    }
+    
+    
+ 
+    system ("pause");
+    return 0;
+}
