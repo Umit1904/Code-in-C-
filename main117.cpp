68709ee98ae7b88e784491052f6c29a3300db76f
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-22 21:01:47 +0600

    Create main117.cpp

---
+++ main117.cpp
@@ -0,0 +1,13 @@
+//117
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int number;
+    int count=0;
+    do{
+        cin>>number;
+        count+=number;
+    }while(number!=0);
+    cout<<count<<endl;
+    return 0;
+}
