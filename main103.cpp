cacf9121c573125653d10121fced9ce0a98e99d8
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-22 18:26:36 +0600

    Create main103.cpp

---
+++ main103.cpp
@@ -0,0 +1,11 @@
+//103
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=100; i<=999;i++){
+        if(pow((i/100%10),3) + pow((i/10%10),3) + pow((i%10),3)==i){
+            cout<<i<<" ";
+            
+        }
+    }
+}
