8e5185448d428a574607bab5e662af942eb2182c
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-22 17:57:12 +0600

    Create main119.cpp

---
+++ main119.cpp
@@ -0,0 +1,12 @@
+//119
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int j=1000; j<=9999; j++){
+        if((j/1000%10)+(j/100%10)+(j/10%10)+(j%10)==15){
+            cout<<j<<" ";
+        }
+    }
+    return 0;
+}
+
