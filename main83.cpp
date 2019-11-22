70e728ac29d6fe8119205a363d1271deeb7fb1ec
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-23 02:58:11 +0600

    Create main83.cpp

---
+++ main83.cpp
@@ -0,0 +1,11 @@
+//83
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    double x, y;
+    for(int i=25; i<=35;i++){
+        x=i+0.5;
+        y=i-0.2;
+        cout<<i<<" "<<x<<" "<<y<<endl;
+    }
+}
