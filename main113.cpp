acef2d9b6e1f3437850a4f1572a44330c6ad2e17
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-22 19:40:29 +0600

    Create main113.cpp

---
+++ main113.cpp
@@ -0,0 +1,14 @@
+//113
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+for (int i=1;i<=20;i++){
+    for (int j=1;j<=10;j++)
+    if(i%2==1)
+       cout<<1<<" ";
+    else
+       cout<<i<<" ";
+    cout<<'\n' ;
+    }
+  
+}
