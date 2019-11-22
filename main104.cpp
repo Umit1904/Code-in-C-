2d4f9086087cbaec4f7a76fd89495340cb4fafe1
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-22 18:31:15 +0600

    Create main104.cpp

---
+++ main104.cpp
@@ -0,0 +1,14 @@
+//104
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int count=0;
+    for(int i=1000; i<=9999;i++){
+        if(((i/1000%10) + (i/100%10) + (i/10%10) + (i%10))*600==i){
+            count++;
+            
+        }
+    }
+    cout<<count<<endl;
+}
+
