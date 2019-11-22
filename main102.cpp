8f2971f6fc05c06a66704315445957ef0e730f87
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-22 18:21:54 +0600

    Create main102.cpp

---
+++ main102.cpp
@@ -0,0 +1,10 @@
+//102
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int j=1000;j<=9999;j++){
+        if((j/1000%10)==3 || (j/100%10)==3 || (j/10%10)==3 || (j%10)==3){
+            cout<<j<<" ";
+        }
+    }
+}
