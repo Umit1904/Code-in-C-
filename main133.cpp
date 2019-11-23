75a5cf5b912de716f709312c030e68ec3a7a75b2
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-23 13:39:06 +0600

    Create main133.cpp

---
+++ main133.cpp
@@ -0,0 +1,17 @@
+//133
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int k;
+    cin>> k;
+for(int n=2;n<=999;n++){
+    if((1+n)*n==2*k){
+      int a=n; 
+        cout<<a;
+         break;
+    }
+    
+}
+
+return 0;
+}
