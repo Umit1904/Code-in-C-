7020b53ef60dc1e249411a934587873d765f2871
Umit1904 <57895446+Umit1904@users.noreply.github.com>
2019-11-23 13:41:39 +0600

    Create main135.cpp

---
+++ main135.cpp
@@ -0,0 +1,12 @@
+//135
+#include <iostream>
+using namespace std;
+int main () {
+    int sum = 0;
+    while (sum != 11) {
+        int a = rand() % 2;
+        cout << a << " ";
+        sum += a;
+    }
+    return 0;
+}
